fb69246456c72b5b2f6bebe76834e9633ce24793
Admin_mschuemi <Admin_mschuemi@its.jnj.com>
2024-03-22 11:05:33 -0400

    Perform pilot simulation study

--- extras/SimulationStudy.R
+++ extras/SimulationStudy.R
@@ -2,6 +2,52 @@
 
 library(MediationAnalysis)
 
+# Pilot simulation study -------------------------------------------------------
+folder <- "PilotSimulation"
+ssList <- list()
+for (yA in log(c(0.5))) {
+  for (mA in log(c(2.0))) {
+    for (yM in log(c(2.0))) {
+      for (confoundingAySd in c(0.5, 2)) {
+        for (confoundingmYSd in c(0.5, 2)) {
+          for (aIntercept in log(c(0.5))) {
+            for (mIntercept in log(c(0.01, 0.1))) {
+              for (yIntercept in log(c(0.01, 0.1))) {
+                confoundingAymSd <- mean(confoundingAySd, confoundingmYSd)
+                ssList[[length(ssList) + 1]] <- createAbstractSimulationSettings(
+                  aIntercept = aIntercept,
+                  confoundingAySd = confoundingAySd,
+                  confoundingmYSd = confoundingmYSd,
+                  confoundingAymSd = confoundingAymSd,
+                  mIntercept = mIntercept,
+                  yIntercept = yIntercept,
+                  yA = yA,
+                  yM = yM,
+                  mA = mA
+                )
+              }              
+            }              
+          }           
+        }
+      }
+    }
+  }
+}
+print(length(ssList))
+msList <- list()
+msList[[length(msList) + 1]] <- createModelsettings(ps = "fit",
+                                                    mrs = "fit",
+                                                    psAdjustment = "matching",
+                                                    mrsAdjustment = "model",
+                                                    mediatorType = "time-to-event")
+
+runSetOfSimulations(folder = folder, 
+                    simulationSettingsList = ssList, 
+                    modelSettingsList = msList,
+                    nSimulations = 1000,
+                    maxCores = 25) 
+
+# Full simulation study --------------------------------------------------------
 folder <- "Simulation"
 ssList <- list()
 for (yA in log(c(0.5, 1.0, 2.0))) {
@@ -9,31 +55,31 @@ for (yA in log(c(0.5, 1.0, 2.0))) {
     for (yM in log(c(0.5, 1.0, 2.0))) {
       for (confoundingAySd in c(0.5, 2)) {
         for (confoundingmYSd in c(0.5, 2)) {
-          for (confoundingAymSd in c(0.5, 2)) {
-            for (aIntercept in log(c(0.1, 0.5, 0.9))) {
-              for (mIntercept in log(c(0.01, 0.1))) {
-                for (yIntercept in log(c(0.01, 0.1))) {
-                  ssList[[length(ssList) + 1]] <- createAbstractSimulationSettings(
-                    aIntercept = aIntercept,
-                    confoundingAySd = confoundingAySd,
-                    confoundingmYSd = confoundingmYSd,
-                    confoundingAymSd = confoundingAymSd,
-                    mIntercept = mIntercept,
-                    yIntercept = yIntercept,
-                    yA = yA,
-                    yM = yM,
-                    mA = mA
-                  )
-                }              
+          for (aIntercept in log(c(0.1, 0.5, 0.9))) {
+            for (mIntercept in log(c(0.01, 0.1))) {
+              for (yIntercept in log(c(0.01, 0.1))) {
+                confoundingAymSd <- mean(confoundingAySd, confoundingmYSd)
+                ssList[[length(ssList) + 1]] <- createAbstractSimulationSettings(
+                  aIntercept = aIntercept,
+                  confoundingAySd = confoundingAySd,
+                  confoundingmYSd = confoundingmYSd,
+                  confoundingAymSd = confoundingAymSd,
+                  mIntercept = mIntercept,
+                  yIntercept = yIntercept,
+                  yA = yA,
+                  yM = yM,
+                  mA = mA
+                )
               }              
-            }           
-          }
+            }              
+          }           
         }
       }
     }
   }
 }
 print(length(ssList))
+
 msList <- list()
 msList[[length(msList) + 1]] <- createModelsettings(ps = "fit",
                                                     mrs = "fit",
@@ -49,6 +95,7 @@ msList[[length(msList) + 1]] <- createModelsettings(ps = "fit",
                                                     psAdjustment = "matching",
                                                     mrsAdjustment = "none",
                                                     mediatorType = "time-to-event")
+
 
 runSetOfSimulations(folder = folder, 
                     simulationSettingsList = ssList, 
